0bda2f2ef06c6590a6c16ce892052f3d5e676330
Jingyi <jingyi012@gmail.com>
2024-04-07 20:17:37 +0800

    firebase config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MOBILE-APPLICATION-PROGRAMMING
+# secondhand_book_selling_platform
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/secondhand_book_selling_platform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.secondhand_book_selling_platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
