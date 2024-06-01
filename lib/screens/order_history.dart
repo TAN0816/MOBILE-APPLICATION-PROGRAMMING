1b6b8450ddfba9addab471ca26ce3a46cb9426fa
chloe <gohning@graduate.utm.my>
2024-06-01 23:24:00 +0800

    update status for order history

--- lib/screens/order_history.dart
+++ lib/screens/order_history.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // Import the intl package
-import 'package:secondhand_book_selling_platform/services/order_service.dart';
+import 'package:intl/intl.dart';
+import 'package:secondhand_book_selling_platform/services/order_history_service.dart';
 import 'package:secondhand_book_selling_platform/model/order.dart' as Orderitem;
 import 'package:secondhand_book_selling_platform/services/user_service.dart';
 
@@ -15,20 +15,17 @@ class MyOrderScreen extends StatefulWidget {
 
 class _MyOrderScreenState extends State<MyOrderScreen> {
   late Future<List<Orderitem.Order>> _ordersFuture;
-  UserService userService = UserService();
-  String userId = '';
-  String bookimage = '';
+  OrderHistoryService orderHistoryService = OrderHistoryService();
 
   @override
   void initState() {
     super.initState();
-    userId = UserService().getUserId;
     _ordersFuture = _fetchOrders(widget.userId);
   }
 
   Future<List<Orderitem.Order>> _fetchOrders(String userId) async {
     try {
-      final orders = await OrderService().getOrder(userId);
+      final orders = await orderHistoryService.getOrderHistory(userId);
       return orders;
     } catch (error) {
       print('Error fetching orders: $error');
@@ -36,11 +33,26 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
     }
   }
 
+  Color _getStatusColor(String status) {
+    switch (status) {
+      case 'received':
+        return Colors.green;
+      case 'cancel':
+        return Colors.red;
+      case 'delivery':
+        return Colors.yellow;
+      case 'pending':
+        return Colors.orange;
+      default:
+        return Colors.black;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(' Orders History'),
+        title: const Text('Orders History'),
       ),
       body: Container(
         color: const Color.fromARGB(255, 247, 247, 247),
@@ -60,8 +72,12 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
                 itemCount: orders.length,
                 itemBuilder: (context, index) {
                   final order = orders[index];
-                  final formattedTimestamp = DateFormat('MM/dd/yyyy')
-                      .format(order.timestamp.toDate()); // Format the timestamp
+                  final formattedTimestamp =
+                      DateFormat('MM/dd/yyyy').format(order.timestamp.toDate());
+
+                  final status = order.status.toLowerCase();
+                  final statusColor = _getStatusColor(status);
+
                   return Padding(
                     padding: const EdgeInsets.fromLTRB(0, 15.0, 0, 6.0),
                     child: Card(
@@ -149,7 +165,7 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
                                             ),
                                           ),
                                           Text(
-                                            'RM${order.totalAmountValue}', // Use actual total amount from order data
+                                            'RM${order.totalAmountValue}',
                                             style: const TextStyle(
                                               fontSize: 14,
                                               fontWeight: FontWeight.w500,
@@ -163,6 +179,29 @@ class _MyOrderScreenState extends State<MyOrderScreen> {
                                   ),
                                 ),
                               ],
+                            ),
+                            const SizedBox(height: 8),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: statusColor
+                                      .withOpacity(0.2), // Light color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust the radius as needed
+                                ),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                child: Text(
+                                  // 'Status: ${order.status}',
+                                  ' ${order.status}',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: statusColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
                             ),
                           ],
                         ),
