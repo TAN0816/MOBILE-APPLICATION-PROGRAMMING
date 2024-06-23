e6646695bb5df34cb91e2694581f3884cca8690f
jiauting <jiautingt@gmail.com>
2024-06-23 13:23:37 +0800

    up

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -5,14 +5,14 @@ import 'package:secondhand_book_selling_platform/model/message.dart'; // Import 
 
 class ChatService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
- 
+
   Future<ChatRoom?> getChatRoomById(String chatRoomId) async {
     try {
-      DocumentSnapshot<Map<String, dynamic>> docSnapshot = await FirebaseFirestore.instance
-          .collection('chatRooms')
-          .doc(chatRoomId)
-          
-          .get();
+      DocumentSnapshot<Map<String, dynamic>> docSnapshot =
+          await FirebaseFirestore.instance
+              .collection('chatRooms')
+              .doc(chatRoomId)
+              .get();
 
       if (!docSnapshot.exists) {
         return null; // Chat room with given ID doesn't exist
@@ -30,48 +30,60 @@ class ChatService {
       return null; // Handle error gracefully
     }
   }
-  
-  Future<void> sendMessage(String chatRoomId, String senderId, String receiverId, String message) async {
-  try {
-    final Timestamp timestamp = Timestamp.now();
 
-    // Create new message object
-    final ChatMessage chatMessage = ChatMessage(
-      id: _firestore.collection('chatRooms').doc(chatRoomId).collection('messages').doc().id,
-      senderId: senderId,
-      receiverId: receiverId,
-      message: message,
-      timestamp: timestamp,
-    );
+  Future<void> sendMessage(String chatRoomId, String senderId,
+      String receiverId, String message) async {
+    try {
+      final Timestamp timestamp = Timestamp.now();
 
-    // Firestore batch to perform multiple operations atomically
-    WriteBatch batch = _firestore.batch();
+      // Create new message object
+      final ChatMessage chatMessage = ChatMessage(
+        id: _firestore
+            .collection('chatRooms')
+            .doc(chatRoomId)
+            .collection('messages')
+            .doc()
+            .id,
+        senderId: senderId,
+        receiverId: receiverId,
+        message: message,
+        timestamp: timestamp,
+      );
 
-    // Step 1: Update the chat room document with latest message details
-    DocumentReference chatRoomDocRef = _firestore.collection('chatRooms').doc(chatRoomId);
-    batch.update(chatRoomDocRef, {
-      'lastMessageTimestamp': timestamp,
-      'users': FieldValue.arrayUnion([senderId, receiverId]), // Add sender and receiver to users array
-      'lastMessage': message,
-      'senderId': senderId, // Update last message field
-    });
+      // Firestore batch to perform multiple operations atomically
+      WriteBatch batch = _firestore.batch();
 
-    // Step 2: Add message to chat room messages subcollection
-    DocumentReference messageDocRef = chatRoomDocRef.collection('messages').doc(chatMessage.id);
-    batch.set(messageDocRef, chatMessage.toMap());
+      // Step 1: Update the chat room document with latest message details
+      DocumentReference chatRoomDocRef =
+          _firestore.collection('chatRooms').doc(chatRoomId);
+      batch.update(chatRoomDocRef, {
+        'lastMessageTimestamp': timestamp,
+        'users': FieldValue.arrayUnion(
+            [senderId, receiverId]), // Add sender and receiver to users array
+        'lastMessage': message,
+        'senderId': senderId, // Update last message field
+      });
 
-    // Commit the batch operation
-    await batch.commit();
+      // Step 2: Add message to chat room messages subcollection
+      DocumentReference messageDocRef =
+          chatRoomDocRef.collection('messages').doc(chatMessage.id);
+      batch.set(messageDocRef, chatMessage.toMap());
 
-    print('Message sent successfully');
-  } catch (e) {
-    print('Error sending message: $e');
-    throw e; // Propagate error to caller
+      // Commit the batch operation
+      await batch.commit();
+
+      print('Message sent successfully');
+    } catch (e) {
+      print('Error sending message: $e');
+      throw e; // Propagate error to caller
+    }
   }
-}
 
-Future<String> getSenderIdFromChatRoom(String chatRoomId) async {
-    DocumentSnapshot chatRoomSnapshot = await FirebaseFirestore.instance.collection('chatRooms').doc(chatRoomId).get();
+  Future<String> getSenderIdFromChatRoom(String chatRoomId) async {
+    DocumentSnapshot chatRoomSnapshot = await FirebaseFirestore.instance
+        .collection('chatRooms')
+        .doc(chatRoomId)
+        .get();
     if (chatRoomSnapshot.exists) {
       List<dynamic> users = chatRoomSnapshot['users'];
       for (String userId in users) {
@@ -82,17 +94,23 @@ Future<String> getSenderIdFromChatRoom(String chatRoomId) async {
     }
     throw Exception('Sender ID not found in chat room');
   }
-  
-Future<ChatRoom> createChatRoom(String sellerId, String currentUserId, String chatRoomId) async {
+
+  Future<ChatRoom> createChatRoom(
+      String sellerId, String currentUserId, String chatRoomId) async {
     try {
       // Example: Creating a chat room document in Firestore
-      DocumentReference<Map<String, dynamic>> chatRoomRef = _firestore.collection('chatRooms').doc(chatRoomId);
+      DocumentReference<Map<String, dynamic>> chatRoomRef =
+          _firestore.collection('chatRooms').doc(chatRoomId);
 
       // Example data for chat room
       Map<String, dynamic> chatRoomData = {
         'id': chatRoomId,
-        'users': [sellerId, currentUserId], // Include both sellerId and currentUserId
-        'lastMessageTimestamp': Timestamp.now(), // Example: Timestamp for last message
+        'users': [
+          sellerId,
+          currentUserId
+        ], // Include both sellerId and currentUserId
+        'lastMessageTimestamp':
+            Timestamp.now(), // Example: Timestamp for last message
         // Add other relevant data as needed
       };
 
@@ -111,6 +129,7 @@ Future<ChatRoom> createChatRoom(String sellerId, String currentUserId, String ch
       rethrow; // Re-throw the error to be handled further up the call stack
     }
   }
+
   // Fetch messages for a specific chat room
   Stream<List<ChatMessage>> getMessages(String chatRoomId) {
     return _firestore
@@ -124,20 +143,44 @@ Future<ChatRoom> createChatRoom(String sellerId, String currentUserId, String ch
             .toList());
   }
 
-
   Future<void> deleteMessage(String chatRoomId, String messageId) async {
     try {
+      // Delete the message document
       await _firestore
           .collection('chatRooms')
           .doc(chatRoomId)
           .collection('messages')
           .doc(messageId)
           .delete();
+
+      // Get the latest message in the chat room
+      QuerySnapshot latestMessagesSnapshot = await _firestore
+          .collection('chatRooms')
+          .doc(chatRoomId)
+          .collection('messages')
+          .orderBy('timestamp', descending: true)
+          .limit(1)
+          .get();
+
+      if (latestMessagesSnapshot.docs.isNotEmpty) {
+        // Update the chat room document with the latest message details
+        await _firestore.collection('chatRooms').doc(chatRoomId).update({
+          'lastMessageTimestamp':
+              latestMessagesSnapshot.docs.first['timestamp'],
+          'lastMessage': latestMessagesSnapshot.docs.first['message'],
+        });
+      } else {
+        // If no messages left, update with default values or empty strings
+        await _firestore.collection('chatRooms').doc(chatRoomId).update({
+          'lastMessageTimestamp': null,
+          'lastMessage': '',
+        });
+      }
+
+      print('Message deleted successfully');
     } catch (e) {
       print('Error deleting message: $e');
       throw Exception('Failed to delete message');
     }
   }
-
 }
-
